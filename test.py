cdcfec5a20a8653d052fac8298d85f10af32dc7e
Harshavardhan0007 <pillariharshavardhan.16@gmail.com>
2026-06-16 12:28:29 +0000

    Modified test.py

--- test.py
+++ test.py
@@ -4,6 +4,6 @@ from app import app
 def test_hello():
     response = app.test_client().get("/")
     assert response.status_code == 200
-    assert response.data == b"Hello world!!!"
-    
+    assert response.data == b"Hello world This Harsha!!!"
+
 
